a14688b97f8b53cb16db7cab3289f9edd722dce5
John T Porvaznik <scurvyjtp@gmail.com>
2020-06-08 16:42:04 -0400

    Day 12 completed.

---
+++ 12a.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "regexp"
+    "strconv"
+)
+
+func check (e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func fileToArray (fn string) []string {
+    var l []string
+    file, err := os.Open(fn)
+    check(err)
+
+    scanner := bufio.NewScanner(file)
+    scanner.Split(bufio.ScanLines)
+    for scanner.Scan() {
+        l = append(l, scanner.Text())
+    }
+
+    file.Close()
+    return l
+}
+
+func main() {
+    fn := "input/infile12"
+    total := 0
+
+    r := regexp.MustCompile(`-?[0-9]+`)
+
+    lines := fileToArray(fn)
+
+    for _, line := range lines {
+        total = 0
+        s := r.FindAllString(line, -1)
+
+        //fmt.Printf("%v:", line)
+
+        for _,t := range s {
+            u,_ := strconv.Atoi(t)
+            total +=u
+        }
+
+        fmt.Printf("\t Total: %v\n", total)
+    }
+}
---
+++ 12b.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "encoding/json"
+)
+
+func check (e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func checkJ(j interface{}) int {
+    n := 0
+
+    switch j := j.(type) {
+
+    case []interface{}:
+        for _,v := range j {
+            n += checkJ(v)
+        }
+    case map[string]interface{} :
+        isRed := false
+
+        for _,v := range j {
+            s, ok := v.(string)
+            if ok && s == "red" {
+                isRed = true
+                break
+            }
+        }
+
+        if !(isRed) {
+            for _,v := range j {
+                n += checkJ(v)
+            }
+        }
+
+    case float64:
+        n = int(j)
+    }
+
+    return n
+}
+
+
+
+func main() {
+    fn := "input/infile12"
+    var j map[string]interface{}
+
+    file, err := ioutil.ReadFile(fn)
+    check(err)
+
+    e := json.Unmarshal([]byte(file), &j)
+    check(e)
+
+    fmt.Printf("%v\n", checkJ(j))
+}
