1bee8eac4bc6e392805409918257d1dc35dde1d0
John T Porvaznik <scurvyjtp@gmail.com>
2020-12-02 14:58:25 -0500

    AoC 2015 Day 20 Part 1.

---
+++ 20a.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "math"
+)
+
+func getProduct(x int, h int) int {
+    return (h / x) * (x*10)
+}
+
+func addArray(div []int) int {
+    var a int
+    for _, d := range div {
+        a += d
+    }
+    //fmt.Printf("  %s : %d\n", div, a)
+    return a
+}
+
+func getDivisorsVal(h int) int {
+    var div []int
+    x := int(math.Sqrt(float64(h)))
+    for i := 1; i <= x; i++ {
+        if h % i == 0 {
+            if (h/i) == i {
+                div = append(div,i)
+            } else {
+                div = append(div,i,(h/i))
+            }
+        }
+    }
+
+    val := addArray(div) * 10
+    return val
+}
+
+func main() {
+    var houses []int
+    target := 29000000
+
+    house := 0
+    houses = append(houses,0)
+
+    for true {
+        house += 1
+        val := getDivisorsVal(house)
+        houses = append(houses,val)
+
+        if val >= target {
+            fmt.Printf("\nFound it!\n\tHouse: %d Ans: %d\n",house,val)
+            os.Exit(0)
+        }
+    }
+}
